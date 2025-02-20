6d4f407087af3024d4c1930a50a4387d6f26209f
Gafarbek <abdusamatovgafarbek7@gmail.com>
2025-02-20 16:18:32 +0500

    first commit

---
+++ Вторая половина/3.0/script.js
@@ -0,0 +1,35 @@
+// let username = 'Gafar'
+// console.log(username);
+// console.log(username.charAt(0));
+
+// console.log(username.length - 1);
+
+// console.log(username[2]);
+
+// console.log(username.at(-1));
+
+// console.log(username.indexOf('G'));
+
+
+// console.log(username.slice());
+
+// const lorem = 'Lorem ipsum dolor, sit amet consecteturf adipisicing elit. Officia itaqueg ratione nostrum, blanditiis incidunt doloribus aliquam vel numquam consequatur dignissimos.'
+
+// const g = lorem.slice(73, 74)
+// const a = lorem.slice(23, 24)
+// const f = lorem.slice(39, 40)
+// const a2 = lorem.slice(41, 42)
+// const r = lorem.slice(2, 3)
+
+// console.log(g + a + f + a2 + r);
+
+
+// let str = 'Hello World'
+
+// str = str.replace(' ',' No ')
+
+// console.log(str);
+
+// console.log(Math.random());
+
+
---
+++ ДЗ/hw 19/index.js
@@ -0,0 +1,26 @@
+// let str = 'aaa@bbb@ccc';
+// str = str.replaceAll('@', '!')
+// console.log(str);
+
+
+// let str = 'HELLOWORLD'
+// let h = str[0] + str.slice(1, 5).toLowerCase() + str[5].toLowerCase() + str.slice(6).toLowerCase();
+// console.log(h);
+
+// let str = 'Hello, it is HTML'
+// let h = str.slice(0, 6) + str.slice(6, 14) + 'not JS';
+// console.log(h);
+
+// let  a = 'alex';
+// let b = a.slice(0, 1).toUpperCase() + a.slice(1, 4);
+// console.log(b);
+
+// let a = Math.random();
+// a = Math.random().toString().replace('.', '');
+// console.log(a);
+
+// const a = 'Lex Luter has a big suit';
+// const b = a.slice(a.indexOf('a'), a.indexOf('a') + 1).toUpperCase();
+// const c = a.slice(a.indexOf('Lex'), a.indexOf('Lex') + 3);
+// console.log(b + c);
+
