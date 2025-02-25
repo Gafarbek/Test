647f371299b647269b07f2e17361bd5073531076
Gafarbek <abdusamatovgafarbek7@gmail.com>
2025-02-25 11:28:02 +0500

    first commit

--- Вторая половина/3.0/script.js
+++ Вторая половина/3.0/script.js
@@ -32,3 +32,97 @@
 // console.log(Math.random());
 
 
+
+
+
+// string 
+
+// console.log(new String());
+
+// split
+
+
+// let username = 'aaa!bbb!ccc';
+// let arr = username.split('!').join('@').split('@').join('9');
+// console.log(arr);
+
+// console.log(username.includes('%'));
+// console.log(username.includes('!'));
+
+// console.log(username.concat('Hello World'));
+
+// console.log(username.endsWith('a'));
+// console.log(username.endsWith('c'));
+
+// console.log(username.startsWith('a'));
+// console.log(username.startsWith('c'));
+
+// console.log(username.repeat(8));
+
+
+// let a = '';
+
+// == 
+// console.log(2 == 3); // false 
+// console.log(2 == 2); // true 
+// console.log(2 == '2'); // true
+// console.log(1 == true); // true
+// console.log(0 == false); // true
+
+// // ===
+// console.log(2 === 3); // false
+// console.log(2 === 2); // true
+// console.log(2 === '2'); // false
+// console.log(1 === true); // true
+
+// console.log(Number(false));
+// console.log(Number('Hello World'));
+// console.log(typeof 5);
+// console.log(typeof NaN);
+// console.log(isNaN(NaN)); // true
+// console.log(isNaN(5)); // false
+
+// 0 = false
+// '' = false
+// false = false
+// null, undefined = false
+
+// 1 = true
+
+
+// <=, >=, <, >
+// console.log(5 <= 5); // true
+// console.log(6 >= 5); // true
+
+// console.log(5 < 5); // false
+// console.log(6 > 5); // true
+
+// // !
+// console.log(5 === 5); // true
+// console.log(5 !== 5); // false
+
+// console.log(!true); // false
+// console.log(!false); // true
+
+
+// console.log(5 != '5'); // false
+// console.log(5 !== '5'); // false
+
+
+// ||, &&, ??
+
+// // ||
+// console.log(5 === 4 || 6 === 3); // false
+// console.log(5 === 4 || 6 === 6); // true
+// console.log(5 === 5 || 6 === 3); // true
+// console.log(5 === 5 || 6 === 6); // true
+
+
+// // &&
+// console.log(5 === 4 && 6 === 3); // false
+// console.log(5 === 4 && 6 === 6); // false
+// console.log(5 === 5 && 6 === 3); // false
+// console.log(5 === 5 && 6 === 6); // true
+
+
+// console.log(null ?? true); // true
